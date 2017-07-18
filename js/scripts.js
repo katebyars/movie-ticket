7db2c26f68defbb27b446d6e026a9ab238b7a971
Kate Byars and Stephanie Mayer <kate@>
2017-07-18 14:05:35 -0700

    fixed output

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,68 @@
 //Business Logic
 function Ticket(names, age, time, titles) {
-  this.names = name;
+  this.names = names;
   this.age = age;
   this.time = time;
   this.titles = titles;
 }
 
+Ticket.prototype.ticketPrice = function (age, time, titles) {
+  //this.age senior = 10
+
+    if (age === "senior") {
+      this.age = 10
+    }  else if (age === "adult") {
+      this.age = 12
+    } else if (age === "child") {
+      this.age = 8
+    }
+
+
+    if (time === "morning") {
+      this.time = 1
+    }
+    else if (time === "afternoon") {
+      this.time = 2
+    }
+    else if (time === "evening") {
+      this.time = 3
+    }
+
+   if (titles === "new-release") {
+     this.titles = 3
+   }
+   else if (titles === "vintage" ) {
+     this.titles = 2
+   }
+   else if (titles === "second-run") {
+     this.titles = 1
+   }
+
+ return price = (this.age + this.time + this.titles);
+}
+
 Ticket.prototype.ticketDetails = function(){
-  return this.names + ", " + this.age + ", " + this.time + ", " + this.titles;
+  return "Guest Name: " + this.names  + " Discount: " + this.age + " Movie Time:  " + this.time + " Movie Type: " + this.titles;
 }
 
 //UI Logic
 $(function(){
-  $(".tickets").submit(function(event){
+  $("#tickets").submit(function(event){
     event.preventDefault();
 
-      var inputtedNames = $('input#names').val();
+      var inputtedNames = $("#names").val();
       var inputtedAge = $("input:radio[name=age]:checked").val();
       var inputtedTime = $("input:radio[name=time]:checked").val();
       var inputtedTitle = $("input:radio[name=titles]:checked").val();
 
+      var newTicketDetails = new Ticket(inputtedNames, inputtedAge, inputtedTime, inputtedTitle);
+      console.log(newTicketDetails);
 
-      var newTicketDetails = (inputtedNames, inputtedAge, inputtedTime, inputtedTitle);
-      alert(newTicketDetails)
 
-  //  $(".outputPrice").empty();
-    newTicketDetails.forEach(function(newTicketDetail){
-      $("ul.outputPrice").append("<li>" + newTicketDetail.ticketDetails() + "</li>");
-    });
-    $('#names').val('');
-    $('.radio').val('');
+      $(".output").text(newTicketDetails.ticketDetails());
+      $(".outputPrice").text("$" +newTicketDetails.ticketPrice(inputtedAge, inputtedTime, inputtedTitle));
+
+     $('#names').val('');
+     $('.radio').val('');
   });
 });
